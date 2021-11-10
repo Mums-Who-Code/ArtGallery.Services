5f7b76851f458f0cbf48ba3b8477f47f22359b27
NaynaMallick <naynamallick@outlook.com>
2021-11-09 21:09:04 -0800

    INFRA: Initialize Unit Test Project

---
+++ ArtGallery.Services.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Xunit;
+
+namespace ArtGallery.Services.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void DeleteMeTest() =>
+            Assert.True(true);
+    }
+}
