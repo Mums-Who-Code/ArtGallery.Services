9abbca5ab51a4e9ea579fe4d4082a5db605404bc
NaynaMallick <naynamallick@outlook.com>
2021-11-11 22:00:02 -0800

    CONTROLLER: Home

---
+++ ArtGallery.Services.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ArtGallery.Services.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello");
+    }
+}
