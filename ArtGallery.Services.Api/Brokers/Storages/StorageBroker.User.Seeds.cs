dd00269d74a7d56fea153a191b753ccb3133e40e
NaynaMallick <naynamallick@outlook.com>
2021-11-29 21:02:09 -0800

    DATA: User Model & Seeding

---
+++ ArtGallery.Services.Api/Brokers/Storages/StorageBroker.User.Seeds.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using ArtGallery.Services.Api.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtGallery.Services.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private static void SeedUsers(ModelBuilder modelBuilder)
+        {
+            var serviceAdminUser = new User
+            {
+                Id = System.Guid.Parse("c3a817cb-1528-4e30-81c0-389297978dc8"),
+                Name = "Admin"
+            };
+
+            modelBuilder.Entity<User>().HasData(serviceAdminUser);
+        }
+    }
+}
--- ArtGallery.Services.Api/Brokers/Storages/StorageBroker.User.cs
+++ ArtGallery.Services.Api/Brokers/Storages/StorageBroker.User.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using ArtGallery.Services.Api.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
 namespace ArtGallery.Services.Api.Brokers.Storages
 {
-    public partial interface IStorageBroker
+    public partial class StorageBroker
     {
+        DbSet<User> Users { get; set; }
     }
 }
