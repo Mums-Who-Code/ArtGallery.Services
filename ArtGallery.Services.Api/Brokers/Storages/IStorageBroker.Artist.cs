c35c6e5d8efc729d492e34ef3a35453c050477b7
NaynaMallick <naynamallick@outlook.com>
2021-12-09 14:31:27 -0800

    BROKER: Insert Artist

--- ArtGallery.Services.Api/Brokers/Storages/IStorageBroker.Artist.cs
+++ ArtGallery.Services.Api/Brokers/Storages/IStorageBroker.Artist.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using ArtGallery.Services.Api.Models.Artists;
+
 namespace ArtGallery.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Artist> InsertArtistAsync(Artist artist);
     }
 }
--- ArtGallery.Services.Api/Brokers/Storages/StorageBroker.Artist.cs
+++ ArtGallery.Services.Api/Brokers/Storages/StorageBroker.Artist.cs
@@ -2,13 +2,27 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using ArtGallery.Services.Api.Models.Artists;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace ArtGallery.Services.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         DbSet<Artist> Artists { get; set; }
+
+        public async ValueTask<Artist> InsertCategoryAsync(Artist artist)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Artist> entityEntry =
+                await broker.Artists.AddAsync(artist);
+
+            await broker.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
