6a6c7d407094d3d6cd9e241c00b79fffa9f1b5c4
NaynaMallick <naynamallick@outlook.com>
2022-01-10 22:23:01 -0800

    ShouldThrowValidationExceptionOnAddIfIdIsInvalidAndLogItAsync  -> FAIL

--- ArtGallery.Services.Tests.Unit/Services/Foundations/Artists/ArtistServiceTests.Validations.Add.cs
+++ ArtGallery.Services.Tests.Unit/Services/Foundations/Artists/ArtistServiceTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using ArtGallery.Services.Api.Models.Artists;
 using ArtGallery.Services.Api.Models.Artists.Exceptions;
@@ -25,6 +26,40 @@ namespace ArtGallery.Services.Tests.Unit.Services.Foundations.Artists
             //when
             ValueTask<Artist> addArtistTask =
                 this.aristService.AddArtistAsync(nullArtist);
+
+            //then
+            await Assert.ThrowsAsync<ArtistValidationException>(() =>
+                addArtistTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(
+                    SameExceptionAs(expectedArtistValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertArtistAsync(It.IsAny<Artist>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidGuid = Guid.Empty;
+            Artist randomArtist = CreateRandomArtist();
+            Artist invalidArtist = randomArtist;
+            invalidArtist.Id = invalidGuid;
+            var invalidArtistException = new InvalidArtistException();
+
+            var expectedArtistValidationException =
+               new ArtistValidationException(invalidArtistException);
+
+            //when
+            ValueTask<Artist> addArtistTask =
+                this.aristService.AddArtistAsync(invalidArtist);
 
             //then
             await Assert.ThrowsAsync<ArtistValidationException>(() =>
